3ff60534ae9705c8ecd8c318d9b976c10dce460f
Hebe3834 <48371210+Hebe3834@users.noreply.github.com>
2021-10-04 23:16:57 -0400

    Create app.py

---
+++ 10_flask/v2/app.py
@@ -0,0 +1,15 @@
+# The Nameless Team: Patrick Ging, Eric Guo, Hebe Huang
+# SoftDev
+# Occupation picker using Flask
+# 2021-10-04
+
+from flask import Flask
+app = Flask(__name__) #create instance of class Flask
+
+@app.route("/")       #assign fxn to route
+def hello_world():
+    print("about to print __name__...")
+    print(__name__)   # this prints in terminal after running/refreshing the site
+    return "No hablo queso!"
+
+app.run()
