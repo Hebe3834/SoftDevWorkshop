dca56885f33c7814a18ab737dd723a08419a2163
Hebe3834 <48371210+Hebe3834@users.noreply.github.com>
2021-10-04 23:17:39 -0400

    Create app.py

---
+++ 10_flask/v4/app.py
@@ -0,0 +1,17 @@
+# The Nameless Team: Patrick Ging, Eric Guo, Hebe Huang
+# SoftDev
+# Occupation picker using Flask
+# 2021-10-04
+
+from flask import Flask
+app = Flask(__name__) #create instance of class Flask
+
+@app.route("/")       #assign fxn to route
+def hello_world():
+    print("the __name__ of this module is... ")
+    print(__name__)
+    return "No hablo queso!"
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
