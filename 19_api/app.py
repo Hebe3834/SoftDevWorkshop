6b133736d22af6c2182f0072721807218a09957b
Hebe3834 <hebeluna00@gmail.com>
2021-11-23 20:31:17 -0500

    wprked on 19_api

---
+++ 19_api/app.py
@@ -0,0 +1,21 @@
+# Justin Zou, Hebe Huang
+# SoftDev
+# K19: A RESTful Journey Skyward
+# 2021-11-23
+
+from flask import Flask, render_template
+import urllib.request
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    nasa = urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=G4M45FjpqLVeBnRGW7ma8GV5qXdOSVjSuw6vgJid')
+    d = json.loads(nasa.read())
+    return render_template("main.html", pic = d["url"], explanation = d["explanation"])
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
