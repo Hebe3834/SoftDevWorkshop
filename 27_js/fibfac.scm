bbc09899fcdec4926e2421c16c128a9a27975282
Hebe3834 <hebeluna00@gmail.com>
2022-02-06 22:41:36 -0500

    rename

---
+++ 27_js/fibfac.scm
@@ -0,0 +1,38 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+
+;; Team HAM :: Hebe Huang, Andrew Juang, Michelle Lo
+;; SoftDev pd2
+;; K27 -- Basic functions in JavaScript
+;; 2022-02-04
+
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname fibfac) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+(define fact
+  (lambda (n)
+    (if (= n 0)
+        1
+        (* (fact (- n 1)) n)
+        )
+    )
+)
+
+
+(define fib
+  (lambda (n)
+    (if (<= n 1)
+        n
+        (+ (fib (- n 1))  (fib (- n 2))
+        )
+    )
+    )
+)
+
+(display (fact 0))
+(display (fact 1))
+(display (fact 2))
+(display (fact 3))
+
+(display (fib 0))
+(display (fib 1))
+(display (fib 2))
+(display (fib 3))
