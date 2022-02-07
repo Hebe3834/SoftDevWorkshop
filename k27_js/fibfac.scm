489f8273cbef4ae6509131876b5d60bafd87600c
Hebe3834 <hebeluna00@gmail.com>
2022-02-06 22:39:02 -0500

    headings

--- k27_js/fibfac.scm
+++ k27_js/fibfac.scm
@@ -1,5 +1,11 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
+
+;; Team HAM :: Hebe Huang, Andrew Juang, Michelle Lo
+;; SoftDev pd2
+;; K27 -- Basic functions in JavaScript
+;; 2022-02-04
+
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname fibfac) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 (define fact
   (lambda (n)
